58c5cf7528913c39223a55c86a0c378dda2e3e3c
Luis Gabriel <luisgabrielmarques12@gmail.com>
2022-09-26 21:01:17 -0300

    Update listas.py

--- python exercicios/listas.py
+++ python exercicios/listas.py
@@ -73,36 +73,103 @@ troca('abcd', 'wxyz')
 'abcde'[:-1]
 
 
+#6. Write a Python program to add 'ing' at the end of a
+#given string (length should be at least 3). If the given
+#string already ends with 'ing' then add 'ly' instead. If the string
+#length of the given string is less than 3, leave it unchanged.
+
+string1 = 'asdaffgs'
+string2 = 'ab'
+string3 = 'abcing'
+
+def add_ing(string):
+ 
+    if len(string) < 3: #testando se tem menos que caracteres
+        resultado = string
+    else:
+        if string[-3:] == 'ing': #testando se termina com ing
+            resultado = string + 'ly' #adicionando 'ly'
+        else:
+            resultado = string + 'ing' #adicionando 'ing'
+    return resultado
+
+add_ing(string1)
+add_ing(string2)
+add_ing(string3)
+
+#7. Write a Python program to find the first
+#appearance of the substring 'not' and 'poor' from a given
+#string, if 'not' follows the 'poor', replace the whole
+#'not'...'poor' substring with 'good'. Return the resulting string.
+
+
+
+def not_poor(string):
+    not_ = string.find('not')
+    poor_ = string.find('poor')
+    if (not_ >= 0 and poor_ > 0) and not_ < poor_:
+        resultado = string.replace(string[not_: poor_ + 4], 'good')
+    else: 
+        resultado = string
+    return resultado
+
+string1 = 'The lyrics is not that poor!'
+string2 = 'The lyrics is poor'
+
+not_poor(string1)
+not_poor(string2)
+not_poor('not poor')
+not_poor('asdnot rigthif askfhjjes adkfns poor kkkkkkk')
 
 
 
 
+#8. Write a Python function that takes a list of words and return the
+#longest word and the length of the longest one.
 
 
+lista = ['mamão', 'varizes', 'papagaiada', 'sol']
+
+def maior_tamanho(lista):
+    maior = 0
+    maior_palavra = ''
+    for c in lista:
+        # vendo se o tamanho da atual palavra é maior que o tamanho da 
+        # maior palavra até então
+        if len(c) > maior:
+            #reatribuindo o tamanho da maior palavra e seu tamanho
+            #caso a condição seja cumprida
+            maior = len(c)
+            maior_palavra = c
+    return (maior_palavra, maior)
+
+print(maior_tamanho(lista))
 
 
+#9. Write a Python program to remove the nth index
+#character from a nonempty string.
+
+string = 'string com n valores'
+
+def apaga_n(string, n):
+    resultado = string[:n] + string[n+1:]
+    return resultado
+
+apaga_n(string, 1)
+apaga_n(string, 0)
 
 
+#10. Write a Python program to change a given string to a new string
+#where the first and last chars have been exchanged.
 
+string = 'string com n valores'
+string2 = 'abcde'
+def mudanca(string):
+    resultado = string[-1] + string[1:-1] + string[0]
+    return resultado
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+mudanca(string)
+mudanca(string2)
 
 
 
